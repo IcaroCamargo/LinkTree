5c5f69d3e7101e960d96c4d1ba02bd56a343db23
IcaroCamargo <icarod331@gmail.com>
2026-03-31 22:29:05 -0300

    Array de links na editpage

--- src/components/EditPage.tsx
+++ src/components/EditPage.tsx
@@ -1,21 +1,68 @@
 import { useState } from "react"
 import { Input } from "./Input"
 
+interface Link {
+    name: string,
+    url: string
+}
+
 export const EditPage = () => {
     const [name, setName] = useState("Zezinho");
+    const [links, setLinks] = useState<Link[]>([]);
+
+    const handleAddLink = () => { setLinks([...links, { name: "", url: "" }]) };
+
+    const handleLinkChange = (index: number, field: string, value: string) => {
+        const updatedLinks = [...links];
+
+        updatedLinks[index] = {
+            ...updatedLinks[index],
+            [field]: value,
+        };
+
+        setLinks(updatedLinks);
+
+    }
+
     return (
         <div>
             <div className="container mx-auto flex">
                 <main className="w-8/12 bg-cyan-900 h-96 p-4 justify-around">
 
-                    <form className="space-y-6">
-                        <Input name={"Nome:"} id={"name"} placeholder=" Ex: Icaro" value={name} onChange={(event) => {setName(event.target.value)}}/>
+                    <form className="space-y-6" onSubmit={(event) => { event.preventDefault(); }}>
+                        <Input name={"Nome:"} id={"name"} placeholder=" Ex: Icaro" value={name} onChange={(event) => { setName(event.target.value) }} />
                         <Input name={"GitHub:"} id={"github"} placeholder=" Ex: IcaroCamargo" />
                         <div className="grid grid-cols-4 gap-4">
                             <Input name={"Linkedin:"} id={"linkedin"} placeholder=" Ex: IcaroCamargo" />
                             <Input name={"Instagram:"} id={"instagram"} placeholder=" Ex: IcaroCamargo" />
                             <Input name={"TikTok:"} id={"tiktok"} placeholder=" Ex: IcaroCamargo" />
                             <Input name={"Facebook:"} id={"facebook"} placeholder=" Ex: IcaroCamargo" />
+                        </div>
+                        <div className="space-y-4">
+                            {
+                                links.map((link, index) => {
+                                    return (
+                                        <div className="flex gap-4">
+                                            <Input
+                                                name={"Link" + index + 1}
+                                                id={"Link-" + index}
+                                                value={link.name}
+                                                placeholder="Ex: Eu deixei???????????" 
+                                                onChange={(event) => {handleLinkChange(index, "name", event.target.value);}}/>
+
+                                            <Input
+                                                name={"URL do Link"}
+                                                id={"link-url-" + index}
+                                                value={link.url}
+                                                placeholder="Ex: https://meusite.com" 
+                                                onChange={(event) => {handleLinkChange(index, "url", event.target.value);}}/>
+                                        </div>
+
+
+                                    )
+                                })
+                            }
+                            <button onClick={handleAddLink} className="bg-cyan-600 w-50 rounded-xl uppercase font-bold italic cursor-pointer px-4 py-2 hover:opacity-90 transition-opacity">Adicionar Link</button>
                         </div>
                     </form>
 
--- src/components/LoginPage.tsx
+++ src/components/LoginPage.tsx
@@ -3,9 +3,9 @@ export function LoginPage() {
 
 
   return (
-    <div className=" w-screen h-screen bg-[#0b1020] flex items-center justify-center">
+    <div className=" w-full h-screen bg-[#0b1020] flex items-center justify-center">
 
-      <div className="z-10 w-full max-w-md bg-[#111a2e] border p-8 rounded-xl shadow border-[#00e5ff]/20 h-96">
+      <div className="z-10 w-full max-w-md bg-[#111a2e] border p-8 rounded-xl shadow border-[#00e5ff]/20">
 
 
         <div className="w-full flex justify-center mb-6">
@@ -27,15 +27,15 @@ export function LoginPage() {
             <label className="text-xs font-bold uppercase tracking-widest text-slate-400 ml-1">email</label>
 
             <div className="relative">
-              <input className="w-full bg-[#0b1020] border-[#173236] border rounded-xl py-3.5 pl-12 pr-4 font-mono text-sm placeholder:text-slate-400" placeholder="dev@etec.sp.gov.br" type="email" value=""/>
+              <input className="w-full bg-[#0b1020] border-[#173236] text-[#00e5ff] border rounded-xl py-3.5 pl-12 pr-4 font-mono text-sm placeholder:text-slate-400" placeholder="dev@etec.sp.gov.br" type="email" />
             </div>
           </div>
           <div className="space-y-2">
             <label className="text-xs font-bold uppercase tracking-widest text-slate-400 ml-1">senha</label>
-            <a className="px-10 font-bold ">esqueci minha senha</a>
+            <a className="text-xs font-bold tracking-widest text-[#00e5ff] uppercase ml-40 hover:underline hover:text-[#00e5ff]">esqueci minha senha</a>
 
             <div className="relative">
-              <input className="w-full bg-[#0b1020] border-[#173236] border rounded-xl py-3.5 pl-12 pr-4 font-mono text-sm placeholder:text-slate-400" placeholder="dev@etec.sp.gov.br" type="email" value=""/>
+              <input className="w-full bg-[#0b1020] border-[#173236] text-[#00e5ff] border rounded-xl py-3.5 pl-12 pr-4 font-mono text-sm placeholder:text-slate-400 mb-8" placeholder="°°°°°°°°" type="password" />
             </div>
           </div>
 
@@ -43,7 +43,11 @@ export function LoginPage() {
         </form>
 
         <div className="flex justify-center">
-          <button className=" bg-blue-900 font-bold border-black border-6 text-xl hover:animate-spin px-2 py-1"> Me aperte</button>
+          <button className="w-full bg-[#00e5ff] hover:bg-[#00e5ff]/90 text-[#0b1020] font-bold py-3 rounded-lg hover:shadow-[0_0_20px_rgba(255,0,0,0.25)]  hover:shadow-[#00E5FF99] transition-shadow mb-3"> Realizar login</button>
+        </div>
+
+        <div>
+          <button className="w-full font-bold bg-[#0b1020] border-[#173236] text-slate-400 border rounded-lg hover:opacity-75 transition-opacity py-3">Realizar Cadastro</button>
         </div>
       </div>
     </div>
